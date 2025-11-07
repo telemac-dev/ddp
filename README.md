bb6742db82657fe67cdcc307185ad40ebd9cc46a
Harold <telemac.dev@gamil.com>
2025-11-07 13:56:21 -0400

    modificar para funcionamento incial do login

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Projeto Django usando Docker e Postgres
 
 Para criar novo app
-docker compose run djangoapp-1 python mangae.py startapp nomeDaApp# erp_pangea
+docker compose run djangoapp python manage.py startapp nomeDaApp# erp_pangea
--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -17,7 +17,7 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-    urlpatterns += static(
-        settings.STATIC_URL, 
-        document_root=settings.STATIC_ROOT)
+    #urlpatterns += static(
+    #    settings.STATIC_URL, 
+    #    document_root=settings.STATIC_ROOT)
     
